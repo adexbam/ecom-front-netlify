d4ef949677e3e6f06c4696cf8aca6e957ac4c2d5
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2023-11-17 13:45:40 +0100

    added a build

---
+++ build/precache-manifest.1c2955cc7943e54e87d82c872e6d543c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5681b5f1d8bf5855180d36b54d231241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9d163cfb2d91802cfaa",
+    "url": "/static/css/main.bd91262d.chunk.css"
+  },
+  {
+    "revision": "55cd36401d409ad8a3c4",
+    "url": "/static/js/2.ef4724f1.chunk.js"
+  },
+  {
+    "revision": "d9d163cfb2d91802cfaa",
+    "url": "/static/js/main.9cd38c24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
